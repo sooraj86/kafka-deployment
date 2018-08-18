7262722cbfa66120a34bb41fb1b7186f747b8555
s0s029n <s0s029n@walmartlabs.com>
2018-08-18 15:55:06 +0530

    adding kafka chef cookbook

---
+++ kafka/metadata.rb
@@ -0,0 +1,11 @@
+name              "kafka"
+maintainer        "sooraj"
+maintainer_email  "X"
+license           "X"
+description       "Installs and configures a Kafka"
+
+supports 'ubuntu'
+
+version           '1.0.0'
+
+chef_version '>= 12.0' if respond_to?(:chef_version)
